92275af4a31fb6d08784789b24cae3c3a0aea6c4
thomasz2221t <56248548+thomasz2221t@users.noreply.github.com>
2021-10-21 15:03:36 +0200

    Dynamiczne dolaczanie dllki assemblerowej

--- JASol/JAApp/JAApp.cpp
+++ JASol/JAApp/JAApp.cpp
@@ -7,14 +7,35 @@
 
 //extern "C" int _stdcall MyProc1(DWORD x, DWORD y);
 
+typedef int(_stdcall* LPFNDLLFUNC1)(DWORD, DWORD);
+//typedef int(_stdcall* LPFNDLLFUNC2)(void);
 
 int main()
 {
-	int x = 3, y = 4, z = 0;
+	int x = 3;
+	int y = 4;
+	int z = 0;
+	HINSTANCE hDLL = LoadLibrary(L"C:\\Users\\0_0\\Documents\\Assembler projekt\\tc2221t_assembler_mandelbrot\\JASol\\Debug\\JADll.dll");
+	//HINSTANCE hDLL = LoadLibrary(L"$ProjectDir\\Debug\\JADll.dll");
+	LPFNDLLFUNC1 lpfnDllFunc1;
+	//LPFNDLLFUNC2 lpfnDllFunc2;
+	if (NULL != hDLL)
+	{
+		lpfnDllFunc1 = (LPFNDLLFUNC1)GetProcAddress(hDLL, "MyProc1");
+		if (NULL != lpfnDllFunc1)
+		{
+			z = lpfnDllFunc1(x, y);
+			std::cout << z << std::endl;
+		}
 
-	z = MyProc1(x, y);  // call to library assemmbler procedure
-	std::cout << z << std::endl;
+		//lpfnDllFunc2 = (LPFNDLLFUNC2)GetProcAddress(hDLL, "MyProc2");
+		//if (NULL != lpfnDllFunc2)
+		//{
+		//	lpfnDllFunc2();
+		//}
+	}
 
+	FreeLibrary(hDLL);
 
 	return 0;
 }
