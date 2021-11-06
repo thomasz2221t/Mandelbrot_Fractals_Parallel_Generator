5d74a375dda0a360d9d8a2ccb971efb9352e6474
thomasz2221t <56248548+thomasz2221t@users.noreply.github.com>
2021-11-06 01:13:22 +0100

    Tworzenie bitmapy i zapisywanie bitmapy do pliku gotowe

--- MandelbrotProject/MandelbrotApp/Form1.cs
+++ MandelbrotProject/MandelbrotApp/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
+using System.Drawing.Imaging;
 
 namespace MandelbrotApp
 {
@@ -25,7 +28,7 @@ namespace MandelbrotApp
         public static extern void MyProc1(ulong[] data);
 
         long iteratorInput; //the input value set by the user as number of iteration cycles
-        int threatsInput; //number of threats to divide the bitmap into chosen by the user
+        int threadsInput; //number of threads to divide the bitmap into chosen by the user
         bool isAsmOrCpp; //if false then asm is set, if true cpp is set
         int resolutionX; //resolution horizontal parameter
         int resolutionY; //resolution vertical parameter 
@@ -53,7 +56,7 @@ namespace MandelbrotApp
 
         private void threatsButton_Click(object sender, EventArgs e)
         {
-            threatsInput = (int)threatsUpDown.Value;
+            threadsInput = (int)threatsUpDown.Value;
         }
 
         private void asmBox_CheckedChanged(object sender, EventArgs e)
@@ -109,15 +112,102 @@ namespace MandelbrotApp
 
         private void proceedButton_Click(object sender, EventArgs e)
         {
+            void thread_function1(object a)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("The thread's numer is {0} i= {1}", a, i);
+                }
+                return;
+            }
+            /*
+            void displayBitmap(Bitmap bitmapObject)
+            {
+                //PictureBox1.Image = bitmapObject;
+
+                // Display the pixel format in Label1.
+                //Label1.Text = "Pixel format: " + bitmapObject.PixelFormat.ToString();
+            }
+            */
+
+            void saveBitmapToFile(Bitmap bitmapObject, string fileSaveDirection)
+            {
+                //bitmapObject.Save(fileSaveDirection, System.Drawing.Imaging.ImageFormat.Bmp);
+                bitmapObject.Save("C:\\Users\\0_0\\Documents\\Assembler_projekt\\tc2221t_assembler_mandelbrot\\MandelbrotProject\\OutputFiles\\MandelbrotFraktal.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                return;
+            }
+
+            Bitmap generateTheBitmap(int resolutionX, int resolutionY)
+            {
+                //Making integer table an bitmap
+                Bitmap bitmapObject = new Bitmap(resolutionX, resolutionY, PixelFormat.Format32bppRgb);
+
+                //reseting color of bitmap
+                for(int y=0; y<resolutionY; y++)
+                {
+                    for(int x=0; x<resolutionX; x++)
+                    {
+                        Color pixelColor = bitmapObject.GetPixel(x, y);
+                        Color newColor = Color.FromArgb(0, 0, 0);//RGB 255 for white colour, RGB 0 for black colour
+                        bitmapObject.SetPixel(x, y, newColor);
+                    }
+                }
+                return bitmapObject;
+            }
+
             iterationButton_Click(sender, EventArgs.Empty);
             threatsButton_Click(sender, EventArgs.Empty);
             resolutionButton_Click(sender, EventArgs.Empty);
             outputButton_Click(sender, EventArgs.Empty);
             Console.WriteLine("iterator: {0}", iteratorInput);
-            Console.WriteLine("threats: {0}", threatsInput);
+            Console.WriteLine("threats: {0}", threadsInput);
             Console.WriteLine("cpp/asm: {0}", (isAsmOrCpp ? "cpp" : "asm"));
             Console.WriteLine("resolution: {0} x {1}", resolutionX, resolutionY);
             Console.WriteLine("file path: {0}", outputFilePath);
+
+            Bitmap bitmapImage = generateTheBitmap(resolutionX,resolutionY);
+
+            saveBitmapToFile(bitmapImage, outputFilePath);
+
+            Thread[] array_of_threads = new Thread[threadsInput]; //tablica do przechowywania wątków
+
+            if(isAsmOrCpp)
+            {
+                //cpp
+                for(int i = 0; i<threadsInput; i++)
+                {
+                    array_of_threads[i] = new Thread(thread_function1);
+                }
+            }
+            else
+            {
+                //asm
+                for(int i = 0; i<threadsInput; i++)
+                {
+                    array_of_threads[i] = new Thread(thread_function1);
+                }
+            }    
+
+            Stopwatch stopWatch = new Stopwatch();//obsluga czasu
+            stopWatch.Start();
+
+            for (int i = 0; i < threadsInput; i++)
+            {
+                array_of_threads[i].Start(2);//rozpoczynamy watki
+                Thread.Sleep(100);
+            }
+
+            //for (int i = 0; i < threadsInput; i++)
+            //{
+
+            //     array_of_threads[i].Join();//synchronizacja watkow (bez sensu jak juz sie koncza
+            //}
+
+            stopWatch.Stop();
+
+            TimeSpan time_period = stopWatch.Elapsed;
+            string elapsedTime = time_period.TotalMilliseconds.ToString();
+            Console.WriteLine("Time of executing dll library in miliseconds {0} for dll library in language: {1}", elapsedTime, (isAsmOrCpp ? "cpp" : "asm"));
 
             int z = Procedura1(3, 4);
             Console.WriteLine("Dll Cpp: {0}", z);
@@ -127,6 +217,7 @@ namespace MandelbrotApp
             Console.WriteLine("{0:X16}", data[0]);
             MyProc1(data);
             Console.WriteLine("{0:X16}", data[0]);
+
         }
     }
 }
